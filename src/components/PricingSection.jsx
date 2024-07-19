c1538855edf8bd7c93c7f4de5f7070335108bd33
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2024-07-19 11:20:01 +0530

    commit

--- src/components/PricingSection.jsx
+++ src/components/PricingSection.jsx
@@ -12,7 +12,7 @@ function PricingSection() {
         </p>
         <div className="text-gray-800">
           <div className="mb-4 inline-block">
-            <a href="#" className="relative flex cursor-pointer items-center pr-12 text-base leading-tight text-purple-600 md:text-xl">
+            {/* <a href="#" className="relative flex cursor-pointer items-center pr-12 text-base leading-tight text-purple-600 md:text-xl">
               See All Features
               <span className="ml-8">
                 <svg
@@ -26,7 +26,7 @@ function PricingSection() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                 </svg>
               </span>
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
@@ -49,7 +49,7 @@ function PricingSection() {
                     className="text-purple-600"
                   ></path>
                 </svg>
-                <p className="py-2 text-xl font-semibold">1 GB</p>
+                <p className="py-2 text-xl font-semibold">Unlimited Tasks</p>
               </li>
               <li className="relative mx-4 mb-2 rounded-md bg-gray-50">
                 <svg className="absolute ml-4 block h-full align-middle" width="17.5px" viewBox="0 0 18 14" fill="none">
@@ -59,23 +59,13 @@ function PricingSection() {
                     className="text-purple-600"
                   ></path>
                 </svg>
-                <p className="py-2 text-xl font-semibold">3 Users</p>
-              </li>
-              <li className="relative mx-4 mb-2 rounded-md bg-gray-50">
-                <svg className="absolute ml-4 block h-full align-middle" width="17.5px" viewBox="0 0 18 14" fill="none">
-                  <path
-                    d="M5.6 10.6L1.4 6.4L0 7.8L5.6 13.4L17.6 1.4L16.2 0L5.6 10.6Z"
-                    fill="currentColor"
-                    className="text-purple-600"
-                  ></path>
-                </svg>
-                <p className="py-2 text-xl font-semibold">1 Brand</p>
+                <p className="py-2 text-xl font-semibold">Per User</p>
               </li>
             </ul>
             <div className="my-10 px-2">
               <a
                 className="block cursor-pointer rounded bg-gray-800 py-4 px-10 text-base leading-tight text-white duration-200 ease-in-out md:inline-block lg:py-4"
-                href="#"
+                href="/"
               >
                 Get Started
               </a>
@@ -87,7 +77,7 @@ function PricingSection() {
             <p className="text-base leading-tight">Most Picked</p>
           </div>
           <div className="relative mx-auto flex max-w-sm flex-col overflow-hidden rounded-md border border-purple-500">
-            <div className="bg-purple-500 py-2 text-xl text-white">Starter</div>
+            <div className="bg-purple-500 py-2 text-xl text-white">Premium</div>
             <div className="py-10 px-4 font-semibold">
               <p>
                 <span className="text-base leading-tight">$</span>79 / month
@@ -103,7 +93,7 @@ function PricingSection() {
                     className="text-purple-600"
                   ></path>
                 </svg>
-                <p className="py-2 text-xl font-semibold">5 GB</p>
+                <p className="py-2 text-xl font-semibold">Unlimited Tasks</p>
               </li>
               <li className="relative mx-4 mb-2 rounded-md bg-gray-50">
                 <svg className="absolute ml-4 block h-full align-middle" width="17.5px" viewBox="0 0 18 14" fill="none">
@@ -113,7 +103,7 @@ function PricingSection() {
                     className="text-purple-600"
                   ></path>
                 </svg>
-                <p className="py-2 text-xl font-semibold">10 Users</p>
+                <p className="py-2 text-xl font-semibold">Per Users</p>
               </li>
               <li className="relative mx-4 mb-2 rounded-md bg-gray-50">
                 <svg className="absolute ml-4 block h-full align-middle" width="17.5px" viewBox="0 0 18 14" fill="none">
@@ -123,7 +113,7 @@ function PricingSection() {
                     className="text-purple-600"
                   ></path>
                 </svg>
-                <p className="py-2 text-xl font-semibold">1 Brand</p>
+                <p className="py-2 text-xl font-semibold">Image Uploading</p>
               </li>
             </ul>
             <div className="my-10 px-2">
@@ -131,58 +121,7 @@ function PricingSection() {
                 className="block cursor-pointer rounded bg-purple-600 py-4 px-10 text-base leading-tight text-white duration-200 ease-in-out md:inline-block lg:py-4"
                 href="#"
               >
-                Free Trial
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="relative text-gray-800">
-          <div className="relative mx-auto flex max-w-sm flex-col overflow-hidden rounded-md border border-purple-100">
-            <div className="bg-purple-50 py-2 text-xl">Team</div>
-            <div className="py-10 px-4 font-semibold">
-              <p>
-                <span className="text-base leading-tight">$</span>279 / month
-              </p>
-            </div>
-            <p className="mx-auto h-24 max-w-xs px-6 text-xl">Billed annually or $349 billed monthly</p>
-            <ul>
-              <li className="relative mx-4 mb-2 rounded-md bg-gray-50">
-                <svg className="absolute ml-4 block h-full align-middle" width="17.5px" viewBox="0 0 18 14" fill="none">
-                  <path
-                    d="M5.6 10.6L1.4 6.4L0 7.8L5.6 13.4L17.6 1.4L16.2 0L5.6 10.6Z"
-                    fill="currentColor"
-                    className="text-purple-600"
-                  ></path>
-                </svg>
-                <p className="py-2 text-xl font-semibold">25 GB</p>
-              </li>
-              <li className="relative mx-4 mb-2 rounded-md bg-gray-50">
-                <svg className="absolute ml-4 block h-full align-middle" width="17.5px" viewBox="0 0 18 14" fill="none">
-                  <path
-                    d="M5.6 10.6L1.4 6.4L0 7.8L5.6 13.4L17.6 1.4L16.2 0L5.6 10.6Z"
-                    fill="currentColor"
-                    className="text-purple-600"
-                  ></path>
-                </svg>
-                <p className="py-2 text-xl font-semibold">25 Users</p>
-              </li>
-              <li className="relative mx-4 mb-2 rounded-md bg-gray-50">
-                <svg className="absolute ml-4 block h-full align-middle" width="17.5px" viewBox="0 0 18 14" fill="none">
-                  <path
-                    d="M5.6 10.6L1.4 6.4L0 7.8L5.6 13.4L17.6 1.4L16.2 0L5.6 10.6Z"
-                    fill="currentColor"
-                    className="text-purple-600"
-                  ></path>
-                </svg>
-                <p className="py-2 text-xl font-semibold">1 Brand</p>
-              </li>
-            </ul>
-            <div className="my-10 px-2">
-              <a
-                className="block cursor-pointer rounded bg-gray-800 py-4 px-10 text-base leading-tight text-white duration-200 ease-in-out md:inline-block lg:py-4"
-                href="#"
-              >
-                Free Trial
+                Buy Now
               </a>
             </div>
           </div>
