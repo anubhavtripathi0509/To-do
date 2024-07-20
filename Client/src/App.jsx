329f10bb9706af929f2cd34a01e0307285a78114
Anubhav Tripathi <anubhavtripathi0509@gmail.com>
2024-07-20 11:54:50 +0530

    commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header';
 import TodoList from './components/TodoList';
-import Login from './components/Login';
 import SignUP from './components/SignUP';
 import PricingSection from './components/PricingSection';
 
+function AuthenticatedApp() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const authToken = localStorage.getItem('authToken');
+        if (!authToken) {
+            navigate('/login');
+        }
+    }, [navigate]);
+
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<TodoList />} />
+                <Route path="/pricing" element={<PricingSection />} />
+                <Route path="/signup" element={<SignUP />} />
+            </Routes>
+        </>
+    );
+}
+
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<TodoList />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/pricing" element={<PricingSection />} />
-        {/* <Route element={<NotFound />} /> */}
-        <Route path="/signup" element={<SignUP />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <AuthenticatedApp />
+        </Router>
+    );
 }
 
 export default App;
--- Server/app.py
+++ Server/app.py
@@ -1,67 +1,83 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, session, abort, redirect, render_template, url_for
 from flask_bcrypt import Bcrypt 
 from flask_cors import CORS, cross_origin
+from authlib.integrations.flask_client import OAuth
+import json
 from models import db, User
- 
+
 app = Flask(__name__)
- 
+
+appConf = {
+    "OAUTH2_CLIENT_ID": "todo",
+    "OAUTH2_CLIENT_SECRET": "dJBlsIp2JIMzBKUN9egNEasuWcHrCkjW",
+    "OAUTH2_ISSUER": "http://localhost:8080/realms/myrealm",
+    "FLASK_SECRET": "cairocoders-ednalan",
+    "FLASK_PORT": 5000
+}
+
 app.config['SECRET_KEY'] = 'cairocoders-ednalan'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskdb.db'
- 
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
-  
+
 bcrypt = Bcrypt(app) 
 CORS(app, supports_credentials=True)
 db.init_app(app)
-  
+
 with app.app_context():
     db.create_all()
- 
+
+oauth = OAuth(app)
+oauth.register(
+    "myApp",
+    client_id=appConf.get("OAUTH2_CLIENT_ID"),
+    client_secret=appConf.get("OAUTH2_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'{appConf.get("OAUTH2_ISSUER")}/.well-known/openid-configuration',
+)
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
- 
+    return render_template("../Client/index.html", session=session.get("user"), pretty=json.dumps(session.get("user"), indent=4))
+
+@app.route("/callback")
+def callback():
+    token = oauth.myApp.authorize_access_token()
+    session["user"] = token
+    return redirect(url_for("hello_world"))
+
+@app.route("/login")
+def login():
+    print("Login route accessed")
+    if "user" in session:
+        print("User already logged in, aborting")
+        abort(404)
+    print("Redirecting to OAuth")
+    return oauth.myApp.authorize_redirect(redirect_uri=url_for("callback", _external=True))
+
+
 @app.route("/signup", methods=["POST"])
 def signup():
     email = request.json["email"]
     password = request.json["password"]
- 
+
     user_exists = User.query.filter_by(email=email).first() is not None
- 
+
     if user_exists:
         return jsonify({"error": "Email already exists"}), 409
-     
     hashed_password = bcrypt.generate_password_hash(password)
     new_user = User(email=email, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
- 
+
     session["user_id"] = new_user.id
- 
+
     return jsonify({
         "id": new_user.id,
         "email": new_user.email
-    })
- 
-@app.route("/login", methods=["POST"])
-def login_user():
-    email = request.json["email"]
-    password = request.json["password"]
-  
-    user = User.query.filter_by(email=email).first()
-  
-    if user is None:
-        return jsonify({"error": "Unauthorized Access"}), 401
-  
-    if not bcrypt.check_password_hash(user.password, password):
-        return jsonify({"error": "Unauthorized"}), 401
-      
-    session["user_id"] = user.id
-  
-    return jsonify({
-        "id": user.id,
-        "email": user.email
     })
 
 @app.route("/logout", methods=["POST"])
@@ -69,6 +85,5 @@ def logout_user():
     session.pop("user_id", None)
     return jsonify({"message": "Logged out successfully"})
 
- 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
